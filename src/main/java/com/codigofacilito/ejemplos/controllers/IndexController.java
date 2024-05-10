1f7c35ef5e1a72b174c5cad5ecd859ef57537bc0
bryanpercy118 <bryanpercy77@gmail.com>
2024-05-09 21:28:09 -0500

    update

--- src/main/java/com/codigofacilito/ejemplos/controllers/IndexController.java
+++ src/main/java/com/codigofacilito/ejemplos/controllers/IndexController.java
@@ -2,12 +2,19 @@ package com.codigofacilito.ejemplos.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
 	@GetMapping(value ="/")
 	public String index(){
+		return "index";
+	}
+	
+	@PostMapping(value="/index-post")
+	public String indexRequestMapping() {
 		return "index";
 	}
 }
