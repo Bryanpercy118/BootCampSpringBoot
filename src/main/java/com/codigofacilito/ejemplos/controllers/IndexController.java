6632cad00a30c7ceef1fa155b7a963a69b35b7a6
bryanpercy118 <bryanpercy77@gmail.com>
2024-05-09 22:19:29 -0500

    update

--- src/main/java/com/codigofacilito/ejemplos/controllers/IndexController.java
+++ src/main/java/com/codigofacilito/ejemplos/controllers/IndexController.java
@@ -1,15 +1,23 @@
 package com.codigofacilito.ejemplos.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class IndexController {
 
 	@GetMapping(value ="/")
-	public String index(){
+	public String index(Model model){
+		model.addAttribute("titulo","Soy el titulo");
+		model.addAttribute("saludo","Soy el saludo");
+		model.addAttribute("show",false);
+		List<String> series = List.of("Dexter","Spiderman", "Vikings");
+		model.addAttribute("series",series);
 		return "index";
 	}
 	
